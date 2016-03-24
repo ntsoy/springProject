32b4be4b04396b63d01e3d62270e3f5b3c94a838
ntsoy <nikolaytsoy90@gmail.com>
2016-03-24 22:59:02 +0600

    home modification

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,15 +9,9 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        ApplicationContext context;
-        context = new FileSystemXmlApplicationContext("C:\\projects\\springProject\\src\\Beans.xml");
-=======
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
->>>>>>> d36f0febc2d3ee2ea0656d963b450f5a09ef39b5
-
         Performer performer = (Performer) context.getBean("kenny2");
         performer.perform();
-        System.out.println("Add new out");
+        System.out.println("Add new out home");
     }
 }
