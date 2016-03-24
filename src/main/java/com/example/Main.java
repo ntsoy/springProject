fb987bc25da70494b55c4a4173754965c0230a1c
ntsoy <nikolaytsoy90@gmail.com>
2016-03-24 23:34:33 +0600

    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 
 
@@ -10,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        Performer performer = (Performer) context.getBean("kenny2");
+        Performer performer = (Performer) context.getBean("hank");
         performer.perform();
-        System.out.println("Add new out work");
+        System.out.println("Spring in ation, page 95");
+
     }
 }
